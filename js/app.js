96db5f6f87c46ec77129d091b631e5bf8a3cc12e
safiyyah <msafiyyah1@gmail.com>
2022-06-28 21:07:16 -0700

    uploading files to landingP

---
+++ js/app.js
@@ -0,0 +1,69 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+const sections = document.querySelectorAll('section');
+const navUl = document.querySelector('#nav__list');
+
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+
+
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+
+
+
+// Add class 'active' to section when near top of viewport
+
+    
+
+
+
+
+
+
+// Scroll to anchor ID using scrollTO event
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+
+
+// Add class 'active' to section when near top of viewport
+
+// Set sections as active
